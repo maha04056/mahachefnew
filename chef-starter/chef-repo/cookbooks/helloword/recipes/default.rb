d8179bdad0a4df8b8c3a943e7b73a7597fa55d2d
kiranbangaru <bangarukiran218@gmail.com>
2017-12-07 12:43:52 +0530

    defult

--- chef-starter/chef-repo/cookbooks/helloword/recipes/default.rb
+++ chef-starter/chef-repo/cookbooks/helloword/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+package 'git' do
+  action :install
+end
+
+package 'tree' do
+  action :install
+end
+
+package 'wget' do
+  action :install
+end
