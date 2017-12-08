960983c6b383fc5ccf71b604ffca69f240f8b726
kiranbangaru <bangarukiran218@gmail.com>
2017-12-08 22:35:50 +0530

    ss

--- chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/deploy.rb
+++ chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/deploy.rb
@@ -4,6 +4,13 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+file '/opt/tomcat/webapps/jenkins.war' do
+  mode '0755'
+  action :delete
+  only_if { ::File.exis?('/opt/tomcat/webapps/jenkins.war')}
+end
+
+
 remote_file '/opt/tomcat/webapps/jenkins.war' do
   source 'https://updates.jenkins-ci.org/download/war/2.76/jenkins.war'
   mode '0755'
