15023c38b80f72f7b0589565fcd3702339950a4d
kiranbangaru <bangarukiran218@gmail.com>
2017-12-08 19:26:32 +0530

    download change

--- chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/downloadtomcat.rb
+++ chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/downloadtomcat.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-remote_file '/home/ubuntu/' do
+remote_file '/home/ubuntu/apache-tomcat-8.0.47.tar.gz' do
   source 'http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
   mode '0755'
   action :create
