a4991536dbc3873d6b969e10df6f540718a54369
kiranbangaru <bangarukiran218@gmail.com>
2017-12-08 23:55:19 +0530

    ss

--- chef-starter/chef-repo/cookbooks/tomcat8u16/attributes/default.rb
+++ chef-starter/chef-repo/cookbooks/tomcat8u16/attributes/default.rb
@@ -1,4 +1,4 @@
 default['tomcat8u16']['tomadmin'] = 'admin'
 default['tomcat8u16']['tompwd'] = 'maha'
 default['tomcat8u16']['tomarole'] = 'manager-gui,admin-gui'
-default['java']['jdk_version'] = '8'
+default['java']['jdk_version'] = '9'
