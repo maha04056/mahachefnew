f517b63ca5413f2ffb35b606f6ba745b4a27cba4
kiranbangaru <bangarukiran218@gmail.com>
2017-12-08 19:52:24 +0530

    download change

--- chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/extract.rb
+++ chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/extract.rb
@@ -3,3 +3,18 @@
 # Recipe:: extract
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+directory '/opt/tomcat' do
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0755'
+  action :create
+end
+
+
+execute 'untarrrr' do
+  command 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+  action :run
+  not_if { ::File.exist?('/opt/tomcat/apache-tomcat-8.0.47/NOTICE')}
+end
+
--- chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/permissions.rb
+++ chef-starter/chef-repo/cookbooks/tomcat8u16/recipes/permissions.rb
@@ -3,3 +3,37 @@
 # Recipe:: permissions
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+execute 'chgrp' do
+  command 'chgrp -R tomcat /opt/tomcat/conf'
+  action :run
+end
+
+execute 'chmod' do
+  command 'chmod g+rwx /opt/tomcat/conf'
+  action :run
+end
+
+execute 'chmod' do
+  command 'chmod g+r /opt/tomcat/conf/*'
+  action :run
+end
+
+execute 'chown' do
+  command 'chown -R tomcat /opt/tomcat/work/'
+  action :run
+end
+
+execute 'chown' do
+  command 'chown -R tomcat /opt/tomcat/temp/'
+  action :run
+end
+
+execute 'chown' do
+  command 'chown -R tomcat /opt/tomcat/logs/'
+  action :run
+end
+
+
+
+
